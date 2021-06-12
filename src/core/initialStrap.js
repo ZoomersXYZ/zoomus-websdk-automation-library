95f65156f039226408b7e625cac2322ce1fe6d2f
Saadh S <atextbooksituation@gmail.com>
2021-06-12 17:59:58 -0400

    Split initialStrap into small functions. Fixed minor logic. Export lib index file

--- src/core/initialStrap.js
+++ src/core/initialStrap.js
@@ -1,12 +1,8 @@
+const cheerio = require( 'cheerio' );
+
 const createLogger = require( '../config/createLogger' );
 
-async function initialStrap( a, name, buttonPage = false, zoomOut = true ) {
-  let sel, rootSel, parentSel, $ = '';
-  const logger = createLogger( `${ name }--initialStrap` );
-
-  logger.info( '-- BEGINNING --' );
-
-  // Optionally refresh when necessary
+async function refreshPage( a ) {
   if ( process.env.RELOAD ) {
     await a.page.reload( { 
       waitUntil: [
@@ -16,48 +12,85 @@ async function initialStrap( a, name, buttonPage = false, zoomOut = true ) {
     ] } );
     await a.page.waitForTimeout( 1000 );
   };
+  return false;
+};
 
-  if ( zoomOut ) {
-    // Mac OS only. macOS 10.15, 11
-    logger.info( 'zoomOut Begin' );
-    await a.page.goto( 'keysmith://run-macro/66B2FA0B-EC68-4546-A16B-97B105FB47F1' );
-    logger.info( 'zoomOut End - any way to confirm?' );
+async function zoomingOut( a, toZoomOut ) {
+  if ( !toZoomOut ) return false;
+
+  logger.info( 'zoomOut Begin' );
+  await a.page.keyboard.down( 'Meta' );
+  await a.page.waitForTimeout( 500 );
+  await a.page.keyboard.press( 'Digit0' );
+  for ( i in [ ...Array( 3 ) ] ) {
+    await a.page.keyboard.press( 'Minus' );
+    await a.page.waitForTimeout( 500 );
   };
+  await a.page.keyboard.up( 'Meta' );
+  logger.info( 'zoomOut End - any way to confirm?' );
+  return true;
+};
 
+async function continueFromIntroFormPage( a, buttonPage ) {
   if ( process.env.RELOAD || buttonPage ) {
-    // Launch meeting page
-    sel = '#root > div > .form > .MuiButtonBase-root > .MuiButton-label';
+    // Launch meeting ourpage
+    const sel = '#root > div > .form > .MuiButtonBase-root > .MuiButton-label';
     const onIntroPage = await a.visibleCheck( sel );
     if ( onIntroPage ) {
       await a.page.waitForTimeout( 1500 );
       await a.selClick( sel );
       await a.page.waitForTimeout( 2500 );
     };
+    return false;
   };
+  return false;
+};
 
-  rootSel = '.join-dialog > div > .zmu-tabs > .zmu-tabs__tab-container';
+async function dismissInitialOverlay( a ) {
+  const rootSel = '.join-dialog > div > .zmu-tabs > .zmu-tabs__tab-container';
   const overlayVisible = await a.selClick( rootSel );
   if ( overlayVisible ) {    
-    sel = ' > .zm-btn';
+    const sel = ' > .zm-btn';
     await a.selClick( rootSel + sel );
   };
+  return false;
+};
 
-  ////
-  // Video Receiving
-  ////
-  parentSel = '#wc-footer div.more-button';
-  sel = parentSel + ' ul li a';
-  const videoReceivingHtml = await page.$eval( sel, e => e.outerHTML );
+async function turnOffVideoReceiving( a ) {
+  const parentSel = '#wc-footer div.more-button';
+  const sel = parentSel + ' ul li a';
+  const videoReceivingHtml = await a.page.$eval( sel, e => e.outerHTML );
   $ = cheerio.load( videoReceivingHtml );
   const videoText = $( 'a' ).text();
 
   if ( videoText == 'Disable video receiving' ) {  
     a.selClick( parentSel );
     await a.page.waitForTimeout( 750 );
-    await page.keyboard.press( 'ArrowDown' );
+    await a.page.keyboard.press( 'ArrowDown' );
     await a.page.waitForTimeout( 1000 );
-    await page.keyboard.press( 'Enter' );
+    await a.page.keyboard.press( 'Enter' );
   };
+};
+
+async function initialStrap( a, name, buttonPage = false, zoomOut = true ) {
+  const logger = createLogger( `${ name }--initialStrap` );
+
+  logger.info( '-- BEGINNING --' );
+
+  // Optionally refresh when necessary
+  await refreshPage( a );
+
+  // Mac OS only. macOS 10.15, 11
+  // await a.page.goto( 'keysmith://run-macro/66B2FA0B-EC68-4546-A16B-97B105FB47F1' );
+
+  // Attempt to zoom out with web automation. May not work if it was not coded before
+  await zoomingOut( a, zoomOut );
+
+  // await continueFromIntroFormPage( a, buttonPage );
+
+  await dismissInitialOverlay( a );
+
+  await turnOffVideoReceiving( a );
 };
 
 module.exports = initialStrap;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const logger = require( './config/logger' );
+const createLogger = require( './config/createLogger' );
+
+const bootstrap = require( './core/bootstrap' );
+module.exports = {
+  bootstrap, 
+};
